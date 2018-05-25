222318531c60322030ff3c215b86f530ab77cd66
araceliflores01 <38224775+araceliflores01@users.noreply.github.com>
2018-05-25 11:36:04 -0500

    6

--- js/acciones.js
+++ js/acciones.js
@@ -2,8 +2,8 @@
 $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){ //escuchador de eventos
 		$('#izquierda').on("swipeleft",function(){ //deslizar ala izquierda
-			navigator.notificacion.alert("Deslizó hacia la izquierda",function(){"aplicación 7","ok"}); 
-		});//cierra swipe izquierda
+			navigator.notification.alert("Deslizó hacia la izquierda",function(){"aplicación 7","ok"}); //aparece mensaje de alert
+		});//cierre swipe izquierda
 		$('#derecha').on("swiperight",function(){
 			navigator.notification.confirm("¿Que quieres hacer?",function(op){ // aparece mensaje alerta
 				switch(op) //valiable op para las oociones
