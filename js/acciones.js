14c6f6e5297733a9ab3109d0a5e95eadd2c3691e
araceliflores01 <38224775+araceliflores01@users.noreply.github.com>
2018-05-25 11:02:30 -0500

    2

--- js/acciones.js
+++ js/acciones.js
@@ -5,15 +5,15 @@ $(document).ready(function(e) {
 			navigator.notificacion.alert("Deslizó hacia la izquierda",function(){"aplicación 7","ok"}); 
 		});//cierra swipe izquierda
 		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Que quieres hacer?",function(op){ // aparece mensaje para
+			navigator.notification.confirm("¿Que quieres hacer?",function(op){ // aparece mensaje alerta
 				switch(op) //valiable op para las oociones
 				{
 					case 1:
-					navigator.notificacion.beep(1); // suena el dispositivo
+					navigator.notificacion.beep(1); // suena dispositivo
 					break;
 					
 					case 2:
-					navigator.notification.vibrate(3000); //vibra el dispositivo
+					navigator.notification.vibrate(3000); //vibra dispositivo
 					break;
 				}
 			},"aplicacion7","Beepear,Vibrar,Cancelar");
